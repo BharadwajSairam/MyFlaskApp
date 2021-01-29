6f5a93a89c2cc0219749c985f0f5998f79b23485
Bharadwaj Sairam <bharadwajsairam001@gmail.com>
2021-01-29 18:41:00 +0000

    Created register route and default register page

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name =request.args.get("name"))
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def greet():
+    return render_template("greet.html", name =request.form.get("name","My Mind"))
