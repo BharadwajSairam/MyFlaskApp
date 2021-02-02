d45d75e47fd760760cf13a7a1a4021e1dffae193
Bharadwaj Sairam <bharadwajsairam001@gmail.com>
2021-02-02 16:34:23 +0000

    created login page and used sessions and cookies to store logged in user

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect
+from flask_session import Session
 
 #inform flask to make the name of the file(application.py) as the flask application
 app=Flask(__name__)
 
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+
+Gender=["Male","Female","Other"]
+Volunteers={}
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if not session.get("name"):
+        return redirect("/login")
+    else:
+        return render_template("index.html")
 
 @app.route("/register", methods=["POST"])
 def greet():
-    return render_template("greet.html", name =request.form.get("name","My Mind"))
+    return render_template("index.html", name =request.form.get("name","My Mind"))
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        session["name"]=request.form.get("Username")
+        print(session.values)
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["name"]=None
+    return redirect("/")
